54628180e7143f627df3b6e0d56a12c2301bbff6
trolleysolingen <j.ankenbrand@gmx.de>
2014-10-17 15:36:56 +0200

    Bugfix

--- app/Controller/ContactController.php
+++ app/Controller/ContactController.php
@@ -1,23 +1,29 @@
-﻿<?php
+<?php
 App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
+
 /**
- * Congregations Controller
+ * Publishers Controller
  *
- * @property Congregation $Congregation
+ * @property Publisher $Publisher
  * @property PaginatorComponent $Paginator
  */
 class ContactController extends AppController {
+
 	public $components = array('PublisherDAO');
 
 	public function beforeFilter() {
-		
+		$publisher = $this->Session->read('publisher');
+		if (!$publisher) {
+			return $this->redirect(array('controller' => 'start', 'action' => 'index'));
+		}
 	}
-		/**
-	 * index method
-	 *
-	 * @return void
-	 */
+
+/**
+ * index method
+ *
+ * @return void
+ */
 	public function index() {
 		$publisher = $this->Session->read('publisher');
 		$this->set("publisher", $publisher);
@@ -25,19 +31,19 @@ class ContactController extends AppController {
 			$contactList = $this->PublisherDAO->getContactPersons($publisher);
 			$this->set("contactList", $contactList);
 		}
-		
+
 		if ($this->request->is('post')) {
 			$useremail = $publisher["Publisher"]["email"];
 			$username = $publisher["Publisher"]["prename"] . " " . $publisher["Publisher"]["surname"];
 			$subject = $this->request->data["Contact"]["subject"];
 			$message = $this->request->data["Contact"]["message"];
-			
+
 			$mail    = new CakeEmail();
 			$result   = $mail->emailFormat('text')
-						->from(array($useremail => $username))
-						->to('info@trolley.jw-center.com')
-						->subject($subject);		
-			
+				->from(array($useremail => $username))
+				->to('info@trolley.jw-center.com')
+				->subject($subject);
+
 			if ($mail ->send($message)) {
 				$this->Session->setFlash('Deine Nachricht wurde abgeschickt. Es wird sich so schnell es geht jemand darum kümmern.', 'default', array('class' => 'alert alert-success'));
 				print($useremail);
@@ -50,4 +56,5 @@ class ContactController extends AppController {
 			}
 		}
 	}
+
 }
