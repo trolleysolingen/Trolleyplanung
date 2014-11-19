83d7e9c202aee7aead3ce32b11d7c22a132dc2e7
trolleysolingen <j.ankenbrand@gmx.de>
2014-11-19 22:21:13 +0100

    Bugfix

--- app/Controller/StartController.php
+++ app/Controller/StartController.php
@@ -25,13 +25,18 @@ class StartController extends AppController {
 		if ($this->request->is('post')) {
 			$email = $this->request->data["Start"]["email"];
 			$password = $this->request->data["Start"]["password"];
-			$publisher = $this->PublisherDAO->getByEmail($email, $password);
-			if (sizeof($publisher) == 0) {
-				$this->Session->setFlash('Der Login war nicht erfolgreich. Bitte überprüfe E-Mail-Adresse und Passwort.', 'default', array('class' => 'alert alert-danger'));
+			if (!empty($email) && !empty($password)) {
+				$publisher = $this->PublisherDAO->getByEmail($email, $password);
+				if (sizeof($publisher) == 0) {
+					$this->Session->setFlash('Der Login war nicht erfolgreich. Bitte überprüfe E-Mail-Adresse und Passwort.', 'default', array('class' => 'alert alert-danger'));
+				} else {
+					$this->Session->write('publisher', $publisher);
+					return $this->redirect(array('controller' => 'reservations', 'action' => 'index'));
+				}
 			} else {
-				$this->Session->write('publisher', $publisher);
-				return $this->redirect(array('controller' => 'reservations', 'action' => 'index'));
+				$this->Session->setFlash('Bitte gib deine E-Mail-Adresse und dein Passwort ein.', 'default', array('class' => 'alert alert-danger'));
 			}
+
 		}
 		$this->set('title_for_layout', 'Trolleyverwaltung');
 	}
